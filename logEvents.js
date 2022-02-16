9e8add0f846f696ebdfa143d650f55c04ebc2faa
Mark Sayers <marksayers46@icloud.com>
2022-02-16 13:05:44 -0600

    imported core modules from node.js

--- logEvents.js
+++ logEvents.js
@@ -2,6 +2,11 @@ const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
 
 const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+
+
 
 console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
 
