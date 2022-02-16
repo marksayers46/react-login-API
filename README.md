03469b9fb1f0b56069c3b0c4bd51c4bf4ed8041d
Mark Sayers <marksayers46@icloud.com>
2022-02-16 13:19:13 -0600

    setup logEvents

---
+++ README.md
@@ -0,0 +1,1 @@
+/ API for my React login app
--- logEvents.js
+++ logEvents.js
@@ -6,8 +6,14 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 
+const logEvent = async (message) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}`;
+    console.log(logItem);
+    try {
+        await fsPromises.appendFile(path.join(__dirname, 'logs', 'event.txt'), logItem);
+    } catch (err) {
+        console.log(err)
+    }
+}
 
-
-console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
-
-console.log(uuid());
